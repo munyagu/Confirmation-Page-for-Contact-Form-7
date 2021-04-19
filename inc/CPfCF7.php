6cb2b7adb7693651943f5a6536f88e0cccd92ece
Shinsuke Nagasawa <nagasawa@barbwire.co.jp>
2021-04-19 18:55:46 +0900

    Fix disable checkbox element

--- inc/CPfCF7.php
+++ inc/CPfCF7.php
@@ -10,22 +10,21 @@ class CPfCF7 {
 
     private static $is_status_confirm = false;
 
-    public static function init(){
+    public static function init() {
 
 
-
-        add_filter( 'wpcf7_form_hidden_fields', [ __CLASS__, 'wpcf7_form_hidden_fields' ] );
+        add_filter('wpcf7_form_hidden_fields', [__CLASS__, 'wpcf7_form_hidden_fields']);
 
         // add_filter( 'wpcf7_before_send_mail', [ __CLASS__, 'wpcf7_before_send_mail' ], 10, 3 );
 
-        add_filter( 'wpcf7_skip_mail', [ __CLASS__, 'wpcf7_skip_mail' ], 10, 2 );
+        add_filter('wpcf7_skip_mail', [__CLASS__, 'wpcf7_skip_mail'], 10, 2);
 
+        add_filter('wpcf7_feedback_response', [__CLASS__, 'wpcf7_feedback_response']);
 
-        add_filter( 'wpcf7_feedback_response', [ __CLASS__, 'wpcf7_feedback_response' ] );
+        add_filter('wpcf7_messages', [__CLASS__, 'wpcf7_messages'], 99);
 
 
     }
-
 
 
     /**
@@ -33,26 +32,25 @@ class CPfCF7 {
      * @param $hidden_fields hidden fields.
      * @return hidden fields.
      */
-    public static function wpcf7_form_hidden_fields( $hidden_fields ){
+    public static function wpcf7_form_hidden_fields($hidden_fields) {
 
         $hidden_fields[self::CONFIRMING_FLAG_FIELD_NAME] = '1';
 
         return $hidden_fields;
     }
 
-    public static function wpcf7_skip_mail( $skip_mail, $contact_form ){
+    public static function wpcf7_skip_mail($skip_mail, $contact_form) {
 
-        if( isset($_POST[ self::CONFIRMING_FLAG_FIELD_NAME ]) && '1' === $_POST[ self::CONFIRMING_FLAG_FIELD_NAME ] ) {
+        if (isset($_POST[self::CONFIRMING_FLAG_FIELD_NAME]) && '1' === $_POST[self::CONFIRMING_FLAG_FIELD_NAME]) {
             $skip_mail = true;
             self::$is_status_confirm = true;
         }
 
 
-
         return $skip_mail;
     }
 
-    public static function wpcf7_before_send_mail( $contact_form, $abort, $submission ){
+    public static function wpcf7_before_send_mail($contact_form, $abort, $submission) {
 
         $abort = true;
 
@@ -60,17 +58,32 @@ class CPfCF7 {
     }
 
 
-
-    public static function wpcf7_feedback_response( $response ){
+    public static function wpcf7_feedback_response($response) {
 
         // error_log(print_r($response,true),3,__DIR__ . '/log.txt');
+        $id = $_POST['_wpcf7'];
+        $contact_form = wpcf7_contact_form($id);
+        $message = $contact_form->message('cpfcf7_confirm');
 
-        if( self::$is_status_confirm ) {
+        if (self::$is_status_confirm) {
             $response['status'] = 'cpfcf7_confirm';
-            $response['message'] = '入力内容をご確認ください。';
+            $response['message'] = $message;
         }
 
 
         return $response;
     }
+
+    public static function wpcf7_messages($messages) {
+
+        $messages['cpfcf7_confirm'] = [
+            'description'
+            => __("Confirmation page is displayed", 'cp-for-cf7'),
+            'default'
+            => __("Please make sure that the information you entered is correct.", 'cp-for-cf7'),
+        ];
+
+        return $messages;
+    }
+
 }
--- js/cpfcf7.js
+++ js/cpfcf7.js
@@ -1,37 +1,88 @@
 window.onload = function () {
     var wpcf7 = document.querySelector( '.wpcf7' );
+
     wpcf7.addEventListener( 'wpcf7submit', function( event ) {
         if('cpfcf7_confirm' === event.detail.status){
-            var elements = document.querySelectorAll( '.wpcf7-form input:not(.wpcf7-submit),.wpcf7-form select,.wpcf7-form textarea' );
-            elements.forEach(function(element) {
-                if( 'hidden' !== element.type ) {
-                    cpfcf7_disable_element(element);
-                }
 
-            });
+            cpfcf7_disable_element(true);
+            var input = document.querySelector('input[name="_cpfcf7_confirm"]');
+            input.value = '0';
+
         }
-    }, false );
+    });
+
+    wpcf7.addEventListener( 'wpcf7invalid', function( event ) {
+        cpfcf7_init_form();
+    });
+    wpcf7.addEventListener( 'wpcf7spam', function( event ) {
+        cpfcf7_init_form();
+    });
+    wpcf7.addEventListener( 'wpcf7mailsent', function( event ) {
+        cpfcf7_init_form();
+    }, false);
+    wpcf7.addEventListener( 'wpcf7mailfailed', function( event ) {
+        cpfcf7_init_form();
+    });
+
 }
-function cpfcf7_disable_element( element ) {
 
-    switch(element.tagName){
-        case 'SELECT':
-            element.disabled = true;
-            break;
+function cpfcf7_init_form(){
+    var input = document.querySelector('input[name="_cpfcf7_confirm"]');
+    input.value = '1';
+    cpfcf7_disable_element(false);
+}
 
-        case 'INPUT':
-            if('radio' === element.type || 'checkbox' === element.type){
-                element.disabled = true;
-            } else {
-                element.readOnly = true;
-            }
-            break;
+function cpfcf7_disable_checkbox( element, disable ){
 
-        case 'TEXTAREA':
-            element.readOnly = true;
+    if(disable){
+        element.addEventListener('click', cpfcf7_disable_click_event);
+    }else{
 
-        default:
-            break;
+        element.removeEventListener('click', cpfcf7_disable_click_event);
     }
+
+}
+
+function cpfcf7_disable_click_event(event){
+    event.preventDefault();
+    return false;
+}
+
+function cpfcf7_disable_element( disable ) {
+
+    var elements = document.querySelectorAll( '.wpcf7-form input:not(.wpcf7-submit),.wpcf7-form select,.wpcf7-form textarea' );
+    elements.forEach(function(element) {
+        if( 'hidden' !== element.type ) {
+            switch(element.tagName){
+                case 'SELECT':
+                    element.disabled = disable;
+                    break;
+
+                case 'INPUT':
+
+                    if('radio' === element.type) {
+                        if( ! element.checked ) {
+                            element.disabled = disable;
+                        }
+                    } else if('checkbox' === element.type) {
+                        cpfcf7_disable_checkbox(element, disable);
+                        if( ! element.checked ) {
+                            element.disabled = disable;
+                        }
+                    } else {
+                        element.readOnly = disable;
+                    }
+
+                    break;
+
+                case 'TEXTAREA':
+                    element.readOnly = disable;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    });
 
 }
